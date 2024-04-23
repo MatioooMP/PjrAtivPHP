975a943f4e7f9fc0af875ab2e1e0a588d07aadd9
MATHEUS MORAES PEREIRA <matheus.mpereira17@senacsp.edu.br>
2024-04-22 22:25:02 -0300

    Terminado function tabuada

--- php/tabuada.php
+++ php/tabuada.php
@@ -1,17 +1,35 @@
 <?php
+function fazerTabuada($numero){
+    $contador = 0;
+    do{
+                $resultado = $numero * $contador;
+        echo "<li>" . $resultado . "</li>";
+        $contador++;
+    } while($contador <= 10);
 
-$numero = $_GET["numero"] ;
+}
+?>
 
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+$numero = intval($_POST["numero"]) ;
 if(is_int($numero) == true){
     fazerTabuada($numero);
 
+}else{
+    echo "Digite um número.";
 }
-function fazerTabuada($numero){
-    do{
-        $contador = 0;
-        $resultado = $numero * $contador;
-        echo "<li>" . $resultado . "</li>";
-        $contador++;
-    } while($contador = 10);
+?>
+</body>
 
-}
+</html>
+
+
+
