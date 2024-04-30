0947d7f24334a7789bd06ad6c6d746174490f93b
MATHEUS MORAES PEREIRA <matheus.mpereira17@senacsp.edu.br>
2024-04-29 22:17:59 -0300

    Terminado tabuada

--- php/tabuada.php
+++ php/tabuada.php
@@ -1,35 +1,42 @@
 <?php
-function fazerTabuada($numero){
-    $contador = 0;
-    do{
-                $resultado = $numero * $contador;
-        echo "<li>" . $resultado . "</li>";
-        $contador++;
-    } while($contador <= 10);
 
+function fazerTabuada($numero)
+{
+
+    $contador = 0;
+
+    if (is_int($numero) == true) {
+        do {
+            $resultado = $numero * $contador;
+            echo  "<tr>" . "<td>" . $numero . "x" . $contador . "=" . $resultado . "</td>" . "</tr>";
+            $contador++;
+        } while ($contador <= 10);
+    } else {
+        echo "Digite um número.";
+    }
 }
+
 ?>
 
 <!DOCTYPE html>
 <html lang="pt-BR">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="./../css/style.css">
 </head>
-<body>
-<?php
-$numero = intval($_POST["numero"]) ;
-if(is_int($numero) == true){
-    fazerTabuada($numero);
 
-}else{
-    echo "Digite um número.";
-}
-?>
+<body>
+    <h1>Tabuada</h1>
+    <table>
+        <?php
+        $numero = intval($_POST["numero"]);
+        fazerTabuada($numero);
+        ?>
+    </table>
+
 </body>
 
 </html>
-
-
-
